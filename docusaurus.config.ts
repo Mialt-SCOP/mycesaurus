67952e86b0138421b2dda0d0c58f28fe52d20c8e
Arthur Lenoir <lenoir.arthur@gmail.com>
2024-04-06 17:12:23 +0200

    fix: production url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://mialt-scop.github.io/mycesaurus/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/mycesaurus/",
