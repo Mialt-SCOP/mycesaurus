30c926cd3b9abe35ffa6d79b0ad1bf9ce3ca50fe
Arthur Lenoir <lenoir.arthur@gmail.com>
2024-04-06 17:13:57 +0200

    fix: prod url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: "https://mialt-scop.github.io/mycesaurus/",
+  url: "https://mialt-scop.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/mycesaurus/",
