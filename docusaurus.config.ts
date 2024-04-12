9a4da78449a8ba355d3de4659b0f288ce3a6df79
Arthur Lenoir <lenoir.arthur@gmail.com>
2024-04-12 17:39:29 +0200

    config pour nouveau domaine

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: "https://mialt-scop.github.io",
+  url: "https://mycetraiteur.fr",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/mycesaurus/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
