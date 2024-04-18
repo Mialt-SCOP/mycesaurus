c82c7e609afff74d17db735cf011246fa20cf4ff
Arthur Lenoir <lenoir.arthur@gmail.com>
2024-04-18 22:15:58 +0200

    fix: preload link

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -9,10 +9,10 @@ export default function FooterWrapper(props: Props): JSX.Element {
   return (
     <>
       <Footer {...props} />
-      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-Regular.woff2").default} type="font/woff2" />
-      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-ExtraBold.woff2").default} type="font/woff2" />
-      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-SemiBold.woff2").default} type="font/woff2" />
-      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/Linotype-Feltpen-Medium.woff2").default} type="font/woff2" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-Regular.woff2").default} type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-ExtraBold.woff2").default} type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-SemiBold.woff2").default} type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/Linotype-Feltpen-Medium.woff2").default} type="font/woff2" crossOrigin="anonymous" />
     </>
   );
 }
