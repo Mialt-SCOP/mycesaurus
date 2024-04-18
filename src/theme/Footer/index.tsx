e7251622985ab627c2710ff3b46290193144772e
Arthur Lenoir <lenoir.arthur@gmail.com>
2024-04-18 22:06:19 +0200

    feat: add prefetch font

---
+++ src/theme/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+import type FooterType from '@theme/Footer';
+import type {WrapperProps} from '@docusaurus/types';
+
+type Props = WrapperProps<typeof FooterType>;
+
+export default function FooterWrapper(props: Props): JSX.Element {
+  return (
+    <>
+      <Footer {...props} />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-Regular.woff2").default} type="font/woff2" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-ExtraBold.woff2").default} type="font/woff2" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/BarlowSemiCondensed-SemiBold.woff2").default} type="font/woff2" />
+      <link rel="preload" fetchpriority="high" as="font" href={require("@site/static/font/Linotype-Feltpen-Medium.woff2").default} type="font/woff2" />
+    </>
+  );
+}
